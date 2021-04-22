fcde221991db1de01b92c76555c4626a4e2ae93e
Sha_code <shannone.controu@gmail.com>
2021-04-22 16:02:04 +0100

    style: responsive home

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }) => (
         <Navigation />
       </div>
       {/* <div className="md:w-3/5 md:my-auto md:h-4/6 w-11/12 h-2/3 mt-10 xs:mt-20 mx-auto"> */}
-      <div className="md:w-3/5 md:my-auto md:h-5/6 w-11/12 h-2/3 mx-auto">
+      <div className="w-11/12 h-2/3 mx-auto mt-5 xs:mt-14 md:w-3/5 md:my-auto md:h-5/6">
         <Component {...pageProps} />
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@
 export default function Home() {
   return (
     <div className="relative bg-gray-50 bg-opacity-20 p-5 md:p-10 rounded-xl h-full shadow-sm">
-      <div className="md:mt-48 md:w-2/5 mt-32 bg-gray-50 bg-opacity-20 p-6 rounded-xl shadow-xl w-4/5 mx-auto">
-        <h1 className="text-3xl py-2 mt-5 text-center">Shannone Controu</h1>
-        <h2 className="text-2xl py-2 mb-5 text-center text-textdark">Web Developer</h2>
+      <div className="md:mt-48 md:w-2/5 mt-14 xs:mt-32 bg-gray-50 bg-opacity-20 p-6 rounded-xl shadow-xl w-4/5 mx-auto">
+        <h1 className="text-xl xs:text-3xl py-2 mt-5 text-center">Shannone Controu</h1>
+        <h2 className="text-lg xs:text-2xl py-2 mb-5 text-center text-textdark">Web Developer</h2>
       </div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-6 rounded-xl top-96 right-7 md:top-32 md:right-32 text-2xl animate-float text-textdark">Front-end</div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-6 rounded-xl -bottom-4 -left-3 md:bottom-24 md:left-24 text-2xl animate-floatFast text-textdark">Reactjs</div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-6 rounded-xl -left-3 -top-2 md:-left-7 text-2xl animate-floatSlow text-textdark">Nextjs</div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl -bottom-6 -right-2 md:bottom-12 md:-right-7 text-lg animate-floatFast">Organized</div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl top-16 left-32 md:top-18 md:left-56 text-lg animate-float">Autonomous</div>
-      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl -top-8 -right-2 md:-top-2 md:-right-3 text-lg animate-floatSlow">Pedagogical</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-4 md:p-6 rounded-xl right-20 xs:top-96 md:top-32 md:right-32 text-xl xs:text-2xl animate-float text-textdark">Front-end</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-4 md:p-6 rounded-xl -bottom-4 -left-3 md:bottom-24 md:left-24 text-xl xs:text-2xl animate-floatFast text-textdark">Reactjs</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-4 md:p-6 rounded-xl -left-3 -top-2 md:-left-7 text-xl xs:text-2xl animate-floatSlow text-textdark">Nextjs</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl -bottom-6 -right-2 md:bottom-12 md:-right-7 xs:text-lg animate-floatFast">Organized</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl top-10 xs:top-16 left-32 md:top-18 md:left-56 xs:text-lg animate-float">Autonomous</div>
+      <div className="absolute bg-gray-50 bg-opacity-20 p-3 rounded-xl shadow-2xl -top-2 xs:-top-8 -right-2 md:-top-2 md:-right-3 xs:text-lg animate-floatSlow">Pedagogical</div>
     </div>
   )
 }
